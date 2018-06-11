32d3a798e45753f2134cd8dd927c93499f5c171f
Lyuha <lyuha@users.noreply.github.com>
2018-06-12 06:29:57 +0900

    Implement converting image to texture in draw area

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,14 @@ fn main() {
 mod feature {
     extern crate find_folder;
     extern crate nfd;
+    extern crate image;
 
-    use conrod;
+    use conrod::{self, widget, Sizeable, Positionable, Widget};
     use conrod::backend::glium::glium::{self, Surface};
     use std;
+    use self::image::Pixel;
+
+    const SCREEN_WIDTH: u32 = 900;
 
     pub fn main() {
         const WIDTH: u32 = 1280;
@@ -43,10 +47,24 @@ mod feature {
         let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
 
         // The image map describing each of our widget->image_mappings (in our case, none).
-        let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+        let mut image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
 
         // Instantiate the generated list of widget identifiers.
         let ids = &mut Ids::new(ui.widget_id_generator());
+
+
+        // let screen = image_to_texture2d(image::RgbaImage::new(SCREEN_WIDTH, HEIGHT), &display);
+        let screen = image_to_texture2d(image::RgbaImage::from_fn(SCREEN_WIDTH, HEIGHT, |x, y| {
+            if x % 2 == 0 {
+                image::Luma([0u8]).to_rgba()
+            } else {
+                image::Luma([255u8]).to_rgba()
+            }
+        }), &display);
+        let (screen_w, screen_h) = screen.dimensions();
+
+        let screen = image_map.insert(screen);
+
 
         let mut events = Vec::new();
 
@@ -97,7 +115,11 @@ mod feature {
                 // Instantiate all widgets in the GUI.
 
                 // Set the widgets.
-                set_widgets(ui.set_widgets(), ids);
+                {
+                    let ui = &mut ui.set_widgets();
+                    set_widgets(ui, ids);
+                    widget::Image::new(screen).w_h(screen_w as conrod::Scalar, screen_h as conrod::Scalar).middle_of(ids.draw_panel).set(ids.draw_texture, ui);
+                }
             }
 
             // Draw the `Ui` if it has changed.
@@ -112,7 +134,7 @@ mod feature {
     }
 
     // Draw the Ui.
-    fn set_widgets(ref mut ui: conrod::UiCell, ids: &mut Ids) {
+    fn set_widgets(ui: &mut conrod::UiCell, ids: &mut Ids) {
         use self::nfd::Response;
 
         use conrod::{color, widget, Colorable, Labelable, Positionable, Sizeable, Widget};
@@ -121,32 +143,57 @@ mod feature {
         widget::Canvas::new()
             .flow_right(&[
                 (
-                    ids.left_column,
-                    widget::Canvas::new().color(color::BLACK).pad(10.0),
+                    ids.draw_panel,
+                    widget::Canvas::new().color(color::BLACK).length(SCREEN_WIDTH as f64),
                 ),
-                (ids.right_column, widget::Canvas::new().color(color::WHITE)),
+                (
+                    ids.control_panel,
+                    widget::Canvas::new()
+                        .color(color::WHITE)
+                        .flow_down(&[
+                            (
+                                ids.button_panel,
+                                widget::Canvas::new()
+                                    .color(color::DARK_GRAY)
+                                    .length(100.0)
+                                    .pad(20.0),
+                            ),
+                            (ids.position_panel, widget::Canvas::new().color(color::GRAY)),
+                        ]),
+                ),
             ])
             .set(ids.master, ui);
 
-        // "Viewing System!" in the middle of the left column.
-        widget::Text::new("Viewing System")
-            .middle_of(ids.left_column)
-            .color(color::WHITE)
-            .font_size(32)
-            .set(ids.text_view, ui);
+        widget::Tabs::new(&[
+            (ids.tab_object, "Object"),
+            (ids.tab_camera, "Camera"),
+            (ids.tab_light, "Light"),
+        ]).wh_of(ids.position_panel)
+            .color(color::LIGHT_BLUE)
+            .label_color(color::DARK_ORANGE)
+            .label_font_size(32)
+            .middle_of(ids.position_panel)
+            .set(ids.tabs, ui);
 
-        widget::Text::new("Hello, conrod!")
-            .middle_of(ids.right_column)
-            .color(color::BLACK)
-            .font_size(32)
-            .set(ids.text_conrod, ui);
+        const BUTTON_DIMENSION: conrod::Dimensions = [100.0, 50.0];
+        const MARGIN: conrod::Scalar = 30.0;
 
         for _press in widget::Button::new()
             .label("Open")
-            .mid_top_of(ids.right_column)
+            .wh(BUTTON_DIMENSION)
+            .mid_left_of(ids.button_panel)
             .set(ids.button_open, ui)
         {
-            let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| panic!(e));
+            let current_path = std::env::current_dir();
+            let path = current_path.as_ref().unwrap().to_str().unwrap();
+
+            println!("{:?}", path);
+
+            let result = nfd::dialog()
+                .filter("dat")
+                .default_path(path)
+                .open()
+                .unwrap_or_else(|e| panic!(e));
 
             match result {
                 Response::Okay(file_path) => println!("File Path = {:?}", file_path),
@@ -154,19 +201,79 @@ mod feature {
                 Response::Cancel => println!("User canceled"),
             }
         }
+
+        for _press in widget::Button::new()
+            .label("Reset")
+            .wh(BUTTON_DIMENSION)
+            .align_middle_y_of(ids.button_open)
+            .align_middle_x_of(ids.button_panel)
+            .parent(ids.button_panel)
+            .set(ids.button_reset, ui)
+        {
+            println!("Press Reset button.");
+        }
+
+        let mut view_mode = ViewMode::Parallel;
+        let is_perspective = view_mode == ViewMode::Perspective;
+        let view_label = if is_perspective {
+            "Perspective"
+        } else {
+            "Parallel"
+        };
+        for _press in widget::Toggle::new(is_perspective)
+            .label(view_label)
+            .wh(BUTTON_DIMENSION)
+            .mid_right_of(ids.button_panel)
+            .align_middle_y_of(ids.button_open)
+            .color(color::WHITE)
+            .set(ids.toggle_view_mode, ui)
+        {
+            view_mode = if is_perspective {
+                ViewMode::Parallel
+            } else {
+                ViewMode::Perspective
+            };
+
+        }
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum ViewMode {
+        Parallel,
+        Perspective,
     }
 
     widget_ids!(
         struct Ids {
             master,
-            left_column,
-            right_column,
-            text_view,
-            text_conrod,
+            draw_panel,
+            control_panel,
+            button_panel,
+            position_panel,
+
+            draw_texture,
 
             button_open,
+            button_reset,
+            toggle_view_mode,
+
+            tabs,
+            tab_object,
+            tab_camera,
+            tab_light,
+
+            label_object,
+            label_camera,
+            label_light,
         }
     );
+
+    fn image_to_texture2d(image: image::RgbaImage, display: &glium::Display) -> glium::texture::Texture2d {
+        let image_dimensions = image.dimensions();
+        let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+        let texture = glium::texture::Texture2d::new(display, raw_image).unwrap();
+        texture
+    }
 }
 
 #[cfg(not(all(feature = "winit", feature = "glium")))]
